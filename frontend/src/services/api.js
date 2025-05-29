bdd78a2fd88fc46fdae20fb8bc4962d2d11dc9dc
Emrah Şander <emrahsander@Emrah-MacBook-Air.local>
2025-05-29 18:23:58 +0300

    server config v5

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.kayliksigorta.com/api/v1/';
+const API_URL = 'https://api.kayaliksigorta.com/api/v1/';
 
 const api = axios.create({
   baseURL: API_URL,
