5e507352a150bfb1702e6334b073aef71c8a95a4
Emrah Şander <emrahsander@Emrah-MacBook-Air.local>
2025-06-11 23:14:52 +0300

    pagination fix 3

--- sigorta_api/settings.py
+++ sigorta_api/settings.py
@@ -188,7 +188,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # REST Framework ayarları
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
